385aa2291d4f6b075058be5eaa6036b063ba9fb9
ulfat-yabroudi <ulfatyab.4@gmail.com>
2022-05-20 15:07:32 +0300

    test1

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 
-Route::view('/about', 'pages.about');
-Route::view('/contact-us', 'pages.contact');
+Route::view('/about', 'pages.about')->name('about');
+Route::view('/contact-us', 'pages.contact')->name('contact');
 
 Route::post('/contact-us', function (Request $request) {
+    $request->validate([
+        'name' => 'required|string|min:3|max:255',
+        'email' => 'required|email',
+        'phone' => 'required|starts_with:9639|digits_between:12,12',
+        'content' => 'required|string|min:5',
+    ]);
+
     $message = new Message();
     $message->name = $request->name;
     $message->email = $request->email;
@@ -38,10 +45,8 @@ Route::get('/admin/messages', function () {
     $messages = Message::all();
 
     return view('messages.index', compact('messages'));
-});
+})->name('messages.store');
 
-Route::get('/admin/messages/{id}', function ($id) {
-    $message = Message::find($id);
-
+Route::get('/admin/messages/{message}', function (Message $message) {
     return view('messages.show', compact('message'));
-});
+})->name('messages.show');
